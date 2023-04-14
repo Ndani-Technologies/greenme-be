99c304cb39c9b350475d0952ea283ecd3610be9e
Ghulam Muhammad <gm@webevis.com>
2023-04-14 17:29:34 +0500

    fix: implemented redis

--- src/Controller/roleController.js
+++ src/Controller/roleController.js
@@ -1,17 +1,32 @@
+const { redisClient } = require("../middleware/redisClient");
 const Role = require("../Models/Role");
 
-const getAllRoles = (req, res, next) => {
-  Role.find({})
-    .populate("permissions")
-    .then(
-      (roles) => {
-        res
-          .status(200)
-          .json({ success: true, message: "Role retrieved", data: roles });
-      },
-      (err) => next(err)
-    )
-    .catch(() => next(new Error("Can't retrieve all roles")));
+const getAllRoles = async (req, res, next) => {
+  try {
+    const cache = await redisClient.get("ROLE");
+    if (cache) {
+      res.status(200).json({
+        success: true,
+        message: "Role retrieved",
+        data: JSON.parse(cache),
+      });
+      return;
+    }
+    Role.find({})
+      .populate("permissions")
+      .then(
+        (roles) => {
+          redisClient.set("ROLE", JSON.stringify(roles));
+          res
+            .status(200)
+            .json({ success: true, message: "Role retrieved", data: roles });
+        },
+        (err) => next(err)
+      )
+      .catch(() => next(new Error("Can't retrieve all roles")));
+  } catch (err) {
+    next(new Error("Coudn't retrieve roles"));
+  }
 };
 
 const createRole = (req, res, next) => {
