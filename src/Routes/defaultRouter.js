b0a3517741d9f89f455a92de97d9ff8ab281cb4d
Ghulam Muhammad <gm@webevis.com>
2023-04-14 15:27:52 +0500

    feat: redis implemented

--- src/Routes/defaultRouter.js
+++ src/Routes/defaultRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const UserRouter = express.Router();
 
 UserRouter.get("/", (req, res) => {
-  res.json({ user: req.user });
+  res.json({ user: "hi" });
 });
 
 module.exports = UserRouter;
